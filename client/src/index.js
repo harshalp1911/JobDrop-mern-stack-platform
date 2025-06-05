a734fac23ce3c2187183b1e77e0e97a82fc84be5
Harshal <harshalp0602@gmail.com>
2025-06-05 20:48:27 +0530

    support react 18 changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
+// client/src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css'; // if you have a global CSS file
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Make sure the HTML has <div id="root"></div>
+// This is the element into which React will render.
+const container = document.getElementById('root');
+
+// React 18+ way:
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
